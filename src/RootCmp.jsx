4ac36e353e4cd08f98d2ee296a62dd40bcf5dc82
gBegoun <galbegun@gmail.com>
2025-03-25 11:59:06 +0200

    Refactor RootCmp layout to include MainHeader and MainSidebar components

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,23 +1,33 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from "./pages/home.jsx"
 import { Board } from "./pages/board.jsx"
-
+import { MainHeader } from "./cmps/MainHeader.jsx"
+import { MainSidebar } from "./cmps/MainSidebar.jsx"
 function RootCmp() {
 
   return (
-    <>
-      <header>
-        {/* You can add navigation links here */}
+    <div className="app-container">
+
+      <header className="main-header">
+        <MainHeader/>
       </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/boards/" element={<Board/>} />
-          <Route path="/boards/:boardId" element={<Board/>} />
-          <Route path="*" element={<h1>404 Not Found</h1>} />
-        </Routes>
-      </main>
-    </>
+
+      <div className="main-layout">
+
+        <aside  className="main-sidebar">
+          <MainSidebar/>
+        </aside>   
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/boards/" element={<Board/>} />
+            <Route path="/boards/:boardId" element={<Board/>} />
+            <Route path="*" element={<h1>404 Not Found</h1>} />
+          </Routes>
+        </main>
+      </div>
+    </div>
   );
 }
 
