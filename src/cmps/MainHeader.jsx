9e61bc62cef746912a1cb4a380bd78f73b8644e0
Ofir-Roz <ofir19923@gmail.com>
2025-03-28 19:40:58 +0300

    add logo to the main header

--- src/cmps/MainHeader.jsx
+++ src/cmps/MainHeader.jsx
@@ -1,7 +1,8 @@
 export const MainHeader = () => {
     return (
         <div>
-            <h1>MainHeader</h1>
+            <img src="../src/assets/styles/icons/logo.png" alt="Logo" style={{ height: '24px', marginRight: '10px', verticalAlign: 'middle' }} />
+            <h1 style={{ display: 'inline', fontSize: '24px' }}>yandom</h1>
         </div>
     );
 };
