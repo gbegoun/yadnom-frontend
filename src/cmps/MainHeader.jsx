4419362f2279073c2b07a29b178de9e30223f442
Ofir-Roz <ofir19923@gmail.com>
2025-03-30 16:06:28 +0300

    refactor MainHeader component; update layout and integrate new icons for notifications and profile

--- src/cmps/MainHeader.jsx
+++ src/cmps/MainHeader.jsx
@@ -1,9 +1,12 @@
-
 export const MainHeader = () => {
     return (
         <div className="main-header">
-            <img src="../src/assets/styles/icons/logo.png" alt="Logo" className="main-header-logo" />
-            <h1 className="main-header-title">yandom</h1>
+            <img src="../src/assets/styles/icons/full_logo.png" alt="Logo" className="main-header-logo" />
+            <div className="main-header-right-group">
+                <img src="../src/assets/styles/icons/notification_bell.svg" alt="Notification" className="main-header-notification" />
+                <img src="../src/assets/styles/icons/dots_cube.svg" alt="Products Switcher" className="main-header-products-switcher" />
+                <img src="../src/assets/styles/icons/default_profile_pic.svg" alt="Profile" className="main-header-profile" />
+            </div>
         </div>
     );
 };
