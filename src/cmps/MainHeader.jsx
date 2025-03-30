d48a1a15f48fd12a3ff5ba8a41f76e032abf5285
Ofir-Roz <ofir19923@gmail.com>
2025-03-30 17:52:07 +0300

    move the icons dir into assets instead of styles; add link to home on the logo img

--- src/cmps/MainHeader.jsx
+++ src/cmps/MainHeader.jsx
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export const MainHeader = () => {
     return (
         <div className="main-header">
-            <img src="../src/assets/styles/icons/full_logo.png" alt="Logo" className="main-header-logo" />
+            <Link to="/">
+                <img src="../src/assets/icons/full_logo.png" alt="Logo" className="main-header-logo" />
+            </Link>
             <div className="main-header-right-group">
-                <img src="../src/assets/styles/icons/notification_bell.svg" alt="Notification" className="main-header-notification" />
-                <img src="../src/assets/styles/icons/dots_cube.svg" alt="Products Switcher" className="main-header-products-switcher" />
-                <img src="../src/assets/styles/icons/default_profile_pic.svg" alt="Profile" className="main-header-profile" />
+                <img src="../src/assets/icons/notification_bell.svg" alt="Notification" className="main-header-notification" />
+                <img src="../src/assets/icons/dots_cube.svg" alt="Products Switcher" className="main-header-products-switcher" />
+                <img src="../src/assets/icons/default_profile_pic.svg" alt="Profile" className="main-header-profile" />
             </div>
         </div>
     );
