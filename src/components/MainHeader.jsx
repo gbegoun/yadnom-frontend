6aec8cd33e05b8eb26f52dc98549548c95a96885
Ofir-Roz <ofir19923@gmail.com>
2025-03-31 18:12:37 +0300

    Remove unused icon files and changed naming for readability

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -7,8 +7,8 @@ export const MainHeader = () => {
                 <img src="../src/assets/icons/full_logo.png" alt="Logo" className="main-header-logo" />
             </Link>
             <div className="main-header-right-group">
-                <img src="../src/assets/icons/notification_bell.svg" alt="Notification" className="main-header-notification" />
-                <img src="../src/assets/icons/dots_cube.svg" alt="Products Switcher" className="main-header-products-switcher" />
+                <img src="../src/assets/icons/notification_bell_icon.svg" alt="Notification" className="main-header-notification" />
+                <img src="../src/assets/icons/dots_cube_icon.svg" alt="Products Switcher" className="main-header-products-switcher" />
                 <img src="../src/assets/icons/default_profile_pic.svg" alt="Profile" className="main-header-profile" />
             </div>
         </div>
