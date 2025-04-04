796f597522b987c288b7588ac73fffd3c8ef9bb4
Ofir-Roz <ofir19923@gmail.com>
2025-04-04 13:56:43 +0300

    move font files and update typography paths; refactor MainHeader and BoardHeader components for improved layout and styling

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -7,9 +7,14 @@ export const MainHeader = () => {
                 <img src="../src/assets/icons/full_logo.png" alt="Logo" className="main-header-logo" />
             </Link>
             <div className="main-header-right-group">
-                <img src="../src/assets/icons/notification_bell_icon.svg" alt="Notification" className="main-header-notification" />
+                <img src="../src/assets/icons/notification_bell_icon.svg" alt="Notification" className="main-header-left-icons" />
+                <img src="../src/assets/icons/invite_members_icon.svg" alt="invite members" className="main-header-left-icons" />
+                |
                 <img src="../src/assets/icons/dots_cube_icon.svg" alt="Products Switcher" className="main-header-products-switcher" />
-                <img src="../src/assets/icons/default_profile_pic.svg" alt="Profile" className="main-header-profile" />
+                <div className='avatar-container'>
+                    <img src="../src/assets/icons/simple_logo.png" alt="Products Switcher" className="main-header-simple-logo" />
+                    <img src="../src/assets/icons/default_profile_pic.svg" alt="Profile" className="main-header-profile" />
+                </div>
             </div>
         </div>
     );
--- src/components/board/BoardHeader.jsx
+++ src/components/board/BoardHeader.jsx
@@ -3,18 +3,25 @@ import { BoardFilter } from './BoardFilter.jsx';
 export const BoardHeader = ({ board }) => {
     return (
         <div className='board-header'>
-            <div className='info-layer'>
-                <h2>{board.name}</h2>
-                <img src="..\src\assets\icons\collapse_group_icon.svg" alt="collapse" />
+            <div className='info-row'>
+                <div className='title-container'>
+                    <h2>{board.name}</h2>
+                    <img src="..\src\assets\icons\collapse_group_icon.svg" alt="collapse" />
+                </div>
                 <div className='users-integration'>
                     activity log
                 </div>
             </div>
             <div className='tables-row'>
-                main table
+                <div className='tables-row-card'>
+                    Main Table
+                    <img src="..\src\assets\icons\options_icon.svg" alt="options" />
+                </div>
+                <div className='add-new-table-card'>
+                    <img src="..\src\assets\icons\add_view_icon.svg" alt="" />
+                </div>
             </div>
-            <hr className='divider' />
-            <div className='filter-plus-new-item-container'>
+            <div className='filter-plus-new-item-row'>
                 <BoardFilter board={board} />
             </div>
         </div>
