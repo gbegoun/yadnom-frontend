d29c24b2a9251e78d9a893e612ecc1a9af866485
Ofir-Roz <ofir19923@gmail.com>
2025-04-07 00:41:17 +0300

    style: update button styles and layout adjustments and boardfilter, make the lines responsive and pixel-perfect

--- src/components/board/BoardFilter.jsx
+++ src/components/board/BoardFilter.jsx
@@ -3,9 +3,35 @@ import { TaskAdd } from "../task/TaskAdd";
 export const BoardFilter = ({ board }) => {
 
     return (
-            <div className="board-filter-container">
-                <TaskAdd/>
-                <span>Search</span> <span>Person</span>
-            </div>
+        <div className="board-filter-container">
+            <TaskAdd />
+            <button className="filter-btn">
+                <img src="..\src\assets\icons\search_icon.svg" alt="search" />
+                Search
+            </button>
+            <button className="filter-btn">
+                <img src="..\src\assets\icons\person_icon.svg" alt="person" />
+                Person
+            </button>
+            <button className="filter-btn">
+                <img src="..\src\assets\icons\filter_icon.svg" alt="filter" />
+                Filter
+            </button>
+            <button className="filter-btn">
+                <img src="..\src\assets\icons\sort_icon.svg" alt="sort" />
+                Sort
+            </button>
+            <button className="filter-btn">
+                <img src="..\src\assets\icons\hide_icon.svg" alt="hide" />
+                Hide
+            </button>
+            <button className="filter-btn">
+                <img src="..\src\assets\icons\group_by_icon.svg" alt="group by" />
+                Group by
+            </button>
+            <button className="filter-btn">
+                <img src="..\src\assets\icons\options_icon.svg" alt="options" />
+            </button>
+        </div>
     );
 };
