de3904e998cd6bf3b545700cfb87177f9b7d8922
gBegoun <@galbegun@gmail.com>
2025-04-08 18:17:57 +0300

    remove log from dropdown

--- src/components/Item/Dropdown.jsx
+++ src/components/Item/Dropdown.jsx
@@ -1,7 +1,5 @@
 export const Dropdown = ({column, value}) => {
     const label = column.settings.options.find(label => label._id === value)?.label || 'Label';
-    console.log(value)
-
     return (
         <div className="dropdown-item">
             <span>{label}</span>
