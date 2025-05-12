31c70b5306036ba07d2aee2a59a470c47f4d2963
Ofir-Roz <ofir19923@gmail.com>
2025-05-12 23:07:52 +0300

    Refactor BoardList to use BoardPreview component

--- src/components/board/BoardList.jsx
+++ src/components/board/BoardList.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import { loadBoards, addNewBoard, removeBoard } from "../../store/actions/board.actions.js"
 import * as ReactRedux from 'react-redux';
+import { BoardPreview } from './BoardPreview';
 
 const { useSelector } = ReactRedux
 
 export const BoardList = () => {
-
     const boards = useSelector(storeState => storeState.boardModule.boards)
 
     const onAddBoard = () => {
@@ -20,7 +20,6 @@ export const BoardList = () => {
         loadBoards();
     }, []);
 
-
     if (!boards) {
         return (<div>Loading...</div>);
     }
@@ -29,28 +28,13 @@ export const BoardList = () => {
         <div className="board-list">
             <button className="add-board-btn" onClick={() => onAddBoard()}>Add Board</button>
             <ul className="board-items">
-                {boards && boards.map(board => {
-                    return (
-                        <a key={board._id} href={`/board/${board._id}`} className="board-item">
-                            <img src="../src/assets/icons/home_page_default_board_img.svg" alt="board image" />
-                            <div className="board-content">
-                                <img src="../src/assets/icons/board_icon.svg" alt="board" className="board-icon" />
-                                <h3>{board.name || board._id}</h3>
-                                <img src="../src/assets/icons/star_favorite.svg" alt="favorite" className="favorite-icon" />
-                                <p>{board.description}</p>
-                            </div>
-                            <div className="board-actions">
-                                <button className="remove-btn"
-                                    onClick={(e) => {
-                                        e.preventDefault();
-                                        onRemoveBoard(board._id);
-                                    }}>
-                                    Remove
-                                </button>
-                            </div>
-                        </a>
-                    );
-                })}
+                {boards && boards.map(board => (
+                    <BoardPreview 
+                        key={board._id}
+                        board={board}
+                        onRemoveBoard={onRemoveBoard}
+                    />
+                ))}
             </ul>
         </div>
     );
