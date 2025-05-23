4b8f62eac4ca02ae28bc71ce8dfe06c331c72db2
gBegoun <@galbegun@gmail.com>
2025-05-23 14:51:19 +0300

    removed colors.js

--- src/constants/colors.js
+++
@@ -1,6 +0,0 @@
-export const GROUP_COLORS = [
-    "#037f4c", "#00c875", "#9cd326", "#cab641", "#ffcb00",
-    "#784bd1", "#9d50dd", "#007eb5", "#579bfc", "#66ccff",
-    "#bb3354", "#df2f4a", "#ff007f", "#ff5ac4", "#ff642e",
-    "#7f5347", "#c4c4c4", "#757575"
-]
