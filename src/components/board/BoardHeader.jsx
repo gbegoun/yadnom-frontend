8d56f15c62c35db3dcd44261e5a816191aeb7415
gBegoun <galbegun@gmail.com>
2025-05-26 16:49:05 +0300

    header sticky

--- src/components/board/BoardHeader.jsx
+++ src/components/board/BoardHeader.jsx
@@ -4,26 +4,28 @@ import SVGService from '../../services/svg/svg.service';
 export const BoardHeader = ({ board }) => {
     return (
         <div className='board-header'>
-            <div className='info-row'>
-                <div className='title-container'>
-                    <h2>{board.name}</h2>
-                    <SVGService.CollapseGroupIcon alt="collapse" />
+            <div className='board-header-container'>
+                <div className='info-row'>
+                    <div className='title-container'>
+                        <h2>{board.name}</h2>
+                        <SVGService.CollapseGroupIcon alt="collapse" />
+                    </div>
+                    <div className='users-integration'>
+                        activity log
+                    </div>
                 </div>
-                <div className='users-integration'>
-                    activity log
+                <div className='tables-row'>
+                    <div className='tables-row-card'>
+                        Main Table
+                        <SVGService.OptionsIcon alt="options" />
+                    </div>
+                    <div className='add-new-table-card'>
+                        <SVGService.AddViewIcon alt="add view" />
+                    </div>
                 </div>
-            </div>
-            <div className='tables-row'>
-                <div className='tables-row-card'>
-                    Main Table
-                    <SVGService.OptionsIcon alt="options" />
+                <div className='filter-plus-new-item-row'>
+                    <BoardFilter board={board} />
                 </div>
-                <div className='add-new-table-card'>
-                    <SVGService.AddViewIcon alt="add view" />
-                </div>
-            </div>
-            <div className='filter-plus-new-item-row'>
-                <BoardFilter board={board} />
             </div>
         </div>
     );
